ac5528b71fd5dbd4e3475ae680117b956ee4af1f
Jonas Hvid <mail@johv.dk>
2020-08-06 18:23:35 +0200

    Add icon to extension
    Closes #4

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ mkdir -p extension/themes
 
 cp src/package.json extension
 cp readme.md extension
+cp icon.png extension
 pipenv run python src/theme.py >extension/themes/parchment-light-color-theme.json
