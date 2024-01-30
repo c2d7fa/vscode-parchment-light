d2c5de7e880122afc1cc7137363adef7404ff0dd
Jonas Hvid <mail@johv.dk>
2024-01-30 15:05:58 +0100

    Update README

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ mkdir -p extension/themes
 cp src/package.json extension
 cp readme.md extension
 cp icon.png extension
-python3 src/theme.py light >extension/themes/parchment-light-color-theme.json
+python3 src/theme.py light >extension/themes/parchment-codex-color-theme.json
 python3 src/theme.py digitized >extension/themes/parchment-digitized-color-theme.json
 python3 src/theme.py candlelight >extension/themes/parchment-candlelight-color-theme.json
 python3 src/theme.py starlight >extension/themes/parchment-starlight-color-theme.json
